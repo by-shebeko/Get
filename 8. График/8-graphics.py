c3bcd11c96d156d7ac872e0a1b39a4a924df1c95
by-shebeko <128597292+by-shebeko@users.noreply.github.com>
2023-05-17 16:12:12 +0300

    Update 8-graphics.py

--- 8. График/8-graphics.py
+++ 8. График/8-graphics.py
@@ -5,26 +5,30 @@ import matplotlib.ticker as ticker
 
 with open('settings.txt') as file:
     settings=[float(i) for i in file.read().split('\n')]
-#считываем показания компаратора и переводим через шаг квантования в вольиты
+
+#читаем показания компаратора и переводим через шаг квантования в вольты
 data=numpy.loadtxt('data.txt', dtype=int) * settings[1]
-#массив времен, создаваемый черед количество измерений и известный шаг по времени
+
+#массив из времен, создаваемый через количество измерений и известный шаг по времени
 data_time=numpy.array([i*settings[0] for i in range(data.size)])
-#параметры фигуры
+
+#параметры графика
 fig, ax=pyplot.subplots(figsize=(16, 10), dpi=500)
 
 #минимальные и максимальные значения для осей
 ax.axis([data.min(), data_time.max()+1, data.min(), data.max()+0.2])
 
-#  Устанавливаем интервал основных делений:
+#интервал основных делений:
 ax.xaxis.set_major_locator(ticker.MultipleLocator(2))
-#  Устанавливаем интервал вспомогательных делений:
+
+#интервал вспомогательных делений:
 ax.xaxis.set_minor_locator(ticker.MultipleLocator(0.5))
 
-#  Тоже самое проделываем с делениями на оси "y":
+#  аналогично для оси "y":
 ax.yaxis.set_major_locator(ticker.MultipleLocator(0.5))
 ax.yaxis.set_minor_locator(ticker.MultipleLocator(0.1))
 
-#название графика с условием для переноса строки и центрированием
+#название графика с условием переноса строки и центровкой
 ax.set_title("\n".join(wrap('процесс заряда и разряда конденсатора в RC цепи', 60)), loc = 'center')
 
 #сетка основная и второстепенная
